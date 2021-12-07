aa3944eac58d6d6dd68e314c01fbb34eef64be8d
Andrea <oquendo.3455@gmail.com>
2021-12-07 14:07:45 -0300

    Usando GET, POST e JSON.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 /* 
 AULA 1
@@ -9,19 +9,61 @@ AULA 1
 - Toda vez que criamos uma aplicação typescript nós precisamos ter um arquivo de configuração do typescript que define quais features do typescript queremos (ou não) utilizar. (npx tsc --init)
 - Para evitar o Ctrl+C e rodar de novo o pacote usamos o pacote ts-node-dev. Sempre que ele ver uma alteração irá executar de novo a aplicação.
 - Agora para evitar ter que ficar escrevendo a rota inteira na hora de mandar um npx etc, escrevemos no package.json um script "dev = rota" e depois npm run dev :D
+- Rota: Endereço completo da requisição
+- Recurso: Qual entidade estamos acessando do sistema.
  */
 
-/* Criando a aplicação */
+// Criando a aplicação
 const app = express();
 
+/* Por padrão o express não sabe que está lidando com uma API REST, e portanto não vem por padrão entendendo JSON. O app.use define uma nova funcionalidade no app. */
+app.use(express.json());
+
 /* 
-- o método GET recebe dois atributos: request, por exemplo, com os dados do usuário, e response, que devolve algo par ao browser.
+- o método GET recebe dois parâmetros: request, por exemplo, com os dados do usuário, e response, que devolve algo par ao browser.
 - para devolver, em vez de uma string, um JSON (JavaScript Object Notation). Geralmente enviamos no responde.json() um array ou um objeto.
+- Cada uma das rotas podem receber parâmetros.
 */
+
+const users = ['Andrea', 'Higidio', 'Patricia'];
+
+// Todos os usuários
 app.get('/users', (request, response)=>{
-    console.log('Listagem de Usuários');
-    
-    response.json(['Andrea', 'Higidio', 'Patricia']);
+    const search = String(request.query.search); // 
+
+    // if ternário
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    return response.json(filteredUsers);
+});
+
+/*
+- Request Param: Parâmetros que vem na própria rota que idenficiam um recurso.
+- Query Param: separado por '?' e geralmente são parâmetros opcionais, por exemplo, para filtros, paginação, etc. Os parâmetros query não são necessáriamente únicos
+- Request Body: Parâmetros para criação e atualização de informações.
+*/
+
+// Retorna um usuário só
+app.get('/users/:id', (request, response) =>{
+    const id = Number(request.params.id); // pega o id do request
+
+    const user = users[id];
+    console.log()
+    return response.json(user);
+})
+
+/*
+O browser não consegue nenhuma rota que não seja get, e por isso usamos o insomnia.rest
+*/
+app.post('/users', (request, response)=>{
+    const data = request.body;
+
+    console.log(data);
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
