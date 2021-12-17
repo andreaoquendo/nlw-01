53d8c1659aa1531746bcaabc0b9c5f4df4ebfa57
Andrea <oquendo.3455@gmail.com>
2021-12-17 13:15:47 -0300

    validacao de dados adicionada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,8 @@
 /* Como o projeto vai ter muitas rotas, é melhor utilizar um arquivo só para as rotas. */
 
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -23,7 +25,22 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 // por que eu posso receber sem os parâmetros?
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post('/points', 
+            upload.single('image'), 
+            celebrate({
+                body: Joi.object().keys({
+                    name: Joi.string().required(),
+                    email: Joi.string().required().email(),
+                    whatsapp: Joi.number().required(),
+                    latitude: Joi.number().required(),
+                    longitude: Joi.number().required(),
+                    city: Joi.string().required(),
+                    uf: Joi.string().required().max(2),
+                    items: Joi.string().required()
+
+                }),
+            }, { abortEarly: false }),
+            pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 // npm install @types/cors -D
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 /* 
 AULA 1
@@ -35,4 +36,5 @@ app.use(routes);
 // nao entendi
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
+app.use(errors());
 app.listen(3334);
